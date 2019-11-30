1ebf96a4d99618c6a59640eacc760d1f2c773e01
Tdxdxoz <tdxdxoz@gmail.com>
2019-11-30 22:11:01 +0800

    完善文字内容

--- zhihu_huawei251.js
+++ zhihu_huawei251.js
@@ -2,7 +2,7 @@ var textpool;
 
 function random_text() {
     let s = '';
-    for(let i = 0; i < 5; ++i)
+    for(let i = 0; i < 8; ++i)
         s += textpool[Math.floor(Math.random() * textpool.length)].textContent + '<br/>';
     return s;
 }
@@ -11,7 +11,7 @@ function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-var text_ans = '抱歉打扰了</p><p>华为离职员工被诬告敲诈勒索罪，羁押251天</p><p>知乎先是降热度，然后又疯狂删问题、删帖</p><p>我尝试用这种方式对抗资乎的言论压制</p><p>不需要任何编程基础，访问 <a href="https://github.com/zhihu-huawei251/zhihu-huawei251">https://github.com/zhihu-huawei251/zhihu-huawei251</a> 获取自动群发脚本，即可参与这场 #行为艺术<br/><br/><br/><br/>';
+var text_ans = '抱歉打扰了</p><p>华为员工离职后被诬告羁押251天，事件发酵后在知乎先是被降热度——明明关注量是热榜第一的十多倍，却只能排到十多名——最后直接被删问题了。我也理解知乎说自己收到了律师函。可是第一，相关内容真的侵犯华为的**合法**权益了吗？真的违反法律法规了吗？第二，即使真的华为法务部门太厉害知乎没办法，用户自发以知乎删不过来的速度发布内容，总不是知乎的错了吧，总不会被华为告了吧(是吗?)。<br/>因此，我决定发起这场行为艺术，用技术和人民的汪洋大海对抗资本对言论、信息自由的压制<br/>不需要任何编程基础，访问 <a href="https://github.com/zhihu-huawei251/zhihu-huawei251">https://github.com/zhihu-huawei251/zhihu-huawei251</a> 获取自动群发脚本，即可参与这场 #行为艺术<br/><br/><br/>以下随机内容，防自动被删：<br/>';
 var re = /question\/(\d+)/;
 
 async function A() {
